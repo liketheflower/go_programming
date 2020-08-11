a68274462c34ad954a744777b6cb320a7b3f816e
liketheflower <jim.morris.shen@gmail.com>
2020-08-11 13:47:51 -0400

    add two go programs

---
+++ hello_world/hello_world1.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"fmt"
+	"math/rand"
+)
+func main(){
+	fmt.Printf("hello, world\n")
+	fmt.Println(rand.Intn(200))
+        fmt.Println(rand.Intn(200))
+        fmt.Println(rand.Intn(200))
+}
